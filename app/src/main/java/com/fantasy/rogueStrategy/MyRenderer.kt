7c59336e798098ff92374ab0307734b971c72874
Djelu <djelu333@gmail.com>
2020-06-26 21:18:30 +0300

    test Triangle

--- app/src/main/java/com/fantasy/rogueStrategy/MyRenderer.kt
+++ app/src/main/java/com/fantasy/rogueStrategy/MyRenderer.kt
@@ -3,11 +3,13 @@ package com.fantasy.rogueStrategy
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.os.SystemClock
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+
 
 class MyRenderer : GLSurfaceView.Renderer {
 
@@ -30,20 +32,30 @@ class MyRenderer : GLSurfaceView.Renderer {
         }                              
     """
 
-
-    /**
-     * Определяем матрицу ВИДА. Её можно рассматривать как камеру. Эта матрица описывает пространство;
-     * она задает положение предметов относительно нашего глаза.
-     */
+    /** Определяем матрицу ВИДА. Её можно рассматривать как камеру. Эта матрица описывает пространство;
+     *  она задает положение предметов относительно нашего глаза.*/
     private val mViewMatrix = FloatArray(16)
+
+    /** Сохраняем матрицу моделей. Она используется для перемещения моделей со своим пространством  (где каждая модель рассматривается
+     *  относительно центра системы координат нашего мира) в пространстве мира.*/
+    private val mModelMatrix = FloatArray(16)
+
     /** Сохраняем матрицу проекции.Она используется для преобразования трехмерной сцены в 2D изображение.  */
+    /** Выделяем массив для хранения объединеной матрицы. Она будет передана в программу шейдера.  */
+    private val mMVPMatrix = FloatArray(16)
+
+    private val mBytesPerFloat = 4 //Количество байт занимаемых одним числом.
+    private val mStrideBytes = 7 * mBytesPerFloat //Количество элементов в вершине.
+    private val mPositionOffset = 0 //Смещение в массиве данных.
+    private val mPositionDataSize = 3 //Размер массива позиций в элементах.
+    private val mColorOffset = 3 //Смещение для данных цвета.
+    private val mColorDataSize = 4 //Размер данных цвета в элементах.
+
+
     private val mProjectionMatrix = FloatArray(16)
 
     /** Будем хранить наши данные в числовом буфере.  */
     private var mTriangleVertices: MutableList<FloatBuffer> = ArrayList()
-
-    /** Количество байт занимаемых одним числом.  */
-    private val mBytesPerFloat = 4
 
     // Треугольники красный, зеленый, и синий.
     private val triangle1VerticesData = floatArrayOf(
@@ -68,20 +80,24 @@ class MyRenderer : GLSurfaceView.Renderer {
 
     private fun compileShader(type: String) : Int{
 
-        val shader =
-            if(type == "vertex") {
-                vertexShader
-            }else{
-                fragmentShader
-            }
+        val shaderCode :String?
+        val shaderType :Int?
+
+        if(type == "vertex") {
+            shaderCode = vertexShader
+            shaderType = GLES20.GL_VERTEX_SHADER
+        }else{
+            shaderCode = fragmentShader
+            shaderType = GLES20.GL_FRAGMENT_SHADER
+        }
 
 
         // Загрузка шейдера.
-        var shaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER)
+        var shaderHandle = GLES20.glCreateShader(shaderType)
 
         if (shaderHandle != 0) {
             // Передаем в наш шейдер программу.
-            GLES20.glShaderSource(shaderHandle, shader)
+            GLES20.glShaderSource(shaderHandle, shaderCode)
 
             // Компиляция шейреда
             GLES20.glCompileShader(shaderHandle)
@@ -147,8 +163,41 @@ class MyRenderer : GLSurfaceView.Renderer {
         Matrix.setLookAtM(rm, rmOffSet, pars[0], pars[1], pars[2], pars[3], pars[4], pars[5], pars[6], pars[7], pars[8])
     }
 
-    private fun frustumM() {
+    private fun frustumM(m: FloatArray, offset: Int, pars: FloatArray) {
+        Matrix.frustumM(m, offset, pars[0], pars[1], pars[2], pars[3], pars[4], pars[5]);
+    }
 
+    /**
+     * Рисуем треугольники из массива данных вершин.
+     *
+     * @param aTriangleBuffer Буфер содержащий данные о вершинах.
+     */
+    private fun drawTriangle(aTriangleBuffer: FloatBuffer) {
+        // Передаем значения о расположении.
+        aTriangleBuffer.position(mPositionOffset)
+        GLES20.glVertexAttribPointer(
+            mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
+            mStrideBytes, aTriangleBuffer
+        )
+        GLES20.glEnableVertexAttribArray(mPositionHandle)
+
+        // Передаем значения о цвете.
+        aTriangleBuffer.position(mColorOffset)
+        GLES20.glVertexAttribPointer(
+            mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
+            mStrideBytes, aTriangleBuffer
+        )
+        GLES20.glEnableVertexAttribArray(mColorHandle)
+
+        // Перемножаем матрицу ВИДА на матрицу МОДЕЛИ, и сохраняем результат в матрицу MVP
+        // (которая теперь содержит модель*вид).
+        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0)
+
+        // Перемножаем матрицу модели*вида на матрицу проекции, сохраняем в MVP матрицу.
+        // (которая теперь содержит модель*вид*проекцию).
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0)
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3)
     }
 
     override fun onSurfaceCreated(glUnused: GL10?, config: EGLConfig?) {
@@ -176,8 +225,6 @@ class MyRenderer : GLSurfaceView.Renderer {
         // Сообщить OpenGL чтобы использовал эту программу при рендеринге.
         GLES20.glUseProgram(programHandle)
 
-
-
         // Инициализируем буфер.
         val triangle = ByteBuffer.allocateDirect(triangle1VerticesData.size * mBytesPerFloat)
             .order(ByteOrder.nativeOrder())
@@ -196,26 +243,32 @@ class MyRenderer : GLSurfaceView.Renderer {
      */
     override fun onSurfaceChanged(glUnused: GL10?, width: Int, height: Int) {
         // Устанавливаем OpenGL окно просмотра того же размера что и поверхность экрана.
-
-        // Устанавливаем OpenGL окно просмотра того же размера что и поверхность экрана.
         GLES20.glViewport(0, 0, width, height)
 
         // Создаем новую матрицу проекции. Высота остается та же,
         // а ширина будет изменяться в соответствии с соотношением сторон.
 
-        // Создаем новую матрицу проекции. Высота остается та же,
-        // а ширина будет изменяться в соответствии с соотношением сторон.
-        val ratio: Float = width.toFloat() / height
-        val left = -ratio
-        val bottom = -1.0f
-        val top = 1.0f
-        val near = 1.0f
-        val far = 10.0f
-
-        Matrix.frustumM(mProjectionMatrix, 0, left, ratio, bottom, top, near, far)
+        val ratio = width.toFloat() / height
+        frustumM(mProjectionMatrix, 0, floatArrayOf(
+            -ratio, ratio, // left, right,
+            -1.0f, 1.0f,   // bottom, top,
+             1.0f, 10.0f   // near, far
+        ))
     }
 
     override fun onDrawFrame(p0: GL10?) {
-        TODO("Not yet implemented")
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT or GLES20.GL_COLOR_BUFFER_BIT)
+
+        // Делаем полный оборот при вращении за 10 секунд.
+        val time = SystemClock.uptimeMillis() % 10000L
+        val angleInDegrees = 360.0f / 10000.0f * time.toInt()
+
+        // Рисуем треугольники плоскостью к нам.
+        Matrix.setIdentityM(mModelMatrix, 0)
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f)
+
+        mTriangleVertices.forEach{
+            drawTriangle(it)
+        }
     }
 }
